b8ad3fe9213dfe3967d9634bdfd059ff3a8507ed
pgonzalezvargas <pgonzalez@genasys.com>
2023-05-23 11:38:35 +0200

    sam local start-api layer path

--- hello/hello.js
+++ hello/hello.js
@@ -1,5 +1,37 @@
 const db = require("/opt/nodejs/db");
+const fs = require('fs');
+const path = require('path');
+// const db = require("/opt/nodejs/db");  // This Path does not exist
+
+function listFilesAndDirectories(directoryPath) {
+    fs.readdir(directoryPath, { withFileTypes: true }, (err, files) => {
+      if (err) {
+        console.error('Error reading directory:', err);
+        return;
+      }
+  
+      files.forEach((file) => {
+        const filePath = path.join(directoryPath, file.name);
+  
+        if (file.isDirectory()) {
+          console.log('Directory:', filePath);
+          listFilesAndDirectories(filePath); // Recursively call the function for subdirectories
+        } else {
+          console.log('File:', filePath);
+        }
+      });
+    });
+  }
 
 exports.lambdaHandler = async (event, context) => {
-    return db.greeting("Hello Lambda");
+    console.log(process.env.NODE_PATH, "NODE PATH NODE PATH");
+    const directoryPath = '/opt';
+    listFilesAndDirectories(directoryPath);
+    console.log("Where is the Layer PAth?")
+    return {
+        statusCode: 200,
+        body: {
+            message: db.greeting("Hello World")
+        }
+    };
 };
